13ff93dc2bb4e55c393e64ab53c86473e533a573
ayeo <me@ayeo.pl>
2019-07-02 21:05:31 +0200

    Add moving

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pygame
+import numpy as np
 
-FPS = 60
+FPS = 30
 SIZE = 30
 TAIL = 5
 
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((SIZE * TAIL, SIZE * TAIL))
+pygame.display.set_caption("Snke")
+clock = pygame.time.Clock()
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, position):
@@ -13,13 +19,42 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = position
         self.image.fill((255, 255, 0))
+        self.moving_x = 0
+        self.moving_y = 1
+
+    def update(self, *args):
+        if self.moving_y == 1:
+            self.position = tuple(np.array(self.position) + (0, 1 * TAIL))
+        if self.moving_y == -1:
+            self.position = tuple(np.array(self.position) + (0, -1 * TAIL))
+        if self.moving_x == 1:
+            self.position = tuple(np.array(self.position) + (1 * TAIL, 0))
+        if self.moving_x == -1:
+            self.position = tuple(np.array(self.position) + (-1 * TAIL, 0))
+
+        self.rect.center = self.position
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.moving_x = -1
+            self.moving_y = 0
+
+        elif keys[pygame.K_RIGHT]:
+            self.moving_x = 1
+            self.moving_y = 0
+
+        elif keys[pygame.K_UP]:
+            self.moving_x = 0
+            self.moving_y = -1
+
+        elif keys[pygame.K_DOWN]:
+            self.moving_x = 0
+            self.moving_y = 1
 
 
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((SIZE * TAIL, SIZE * TAIL))
-pygame.display.set_caption("Snke")
-clock = pygame.time.Clock()
+
 
 
 player = Player((50, 50))
@@ -38,6 +73,14 @@ while running:
 
     screen.fill((0, 0, 0))
 
+    player.move()
+
+    keys = pygame.key.get_pressed()
+    events = pygame.event.get()
+    print(events)
+
+    for event in pygame.event.get():
+        print(event)
     sprites.update()
     sprites.draw(screen)
 
